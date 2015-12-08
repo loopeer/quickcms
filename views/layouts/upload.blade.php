b01b2e6c8f6daed83a5f441e0233205790245d14
dengyongbin <dengyongbin@loopeer.com>
2015-12-08 17:10:37 +0800

    update upload

--- views/layouts/upload.blade.php
+++ views/layouts/upload.blade.php
@@ -1,16 +1,16 @@
-<link rel="stylesheet" type="text/css" href="{{{ asset('backend/js/blueimp/css/jquery.fileupload-ui.css') }}}">
-<link rel="stylesheet" type="text/css" href="{{{ asset('backend/js/blueimp/css/blueimp-gallery.min.css') }}}">
-<script src="{{ asset('backend/js/blueimp/tmpl.min.js') }}"></script>
-<script src="{{ asset('backend/js/blueimp/load-image.min.js') }}"></script>
-<script src="{{ asset('backend/js/blueimp/canvas-to-blob.min.js') }}"></script>
-<script src="{{ asset('backend/js/blueimp/bootstrap-image-gallery.min.js') }}"></script>
-<script src="{{ asset('backend/js/blueimp/jquery.iframe-transport.js') }}"></script>
-<script src="{{ asset('backend/js/blueimp/jquery.fileupload.js') }}"></script>
-<script src="{{ asset('backend/js/blueimp/jquery.fileupload-process.js') }}"></script>
-<script src="{{ asset('backend/js/blueimp/jquery.fileupload-image.js') }}"></script>
-<script src="{{ asset('backend/js/blueimp/jquery.fileupload-validate.js') }}"></script>
-<script src="{{ asset('backend/js/blueimp/jquery.fileupload-ui.js') }}"></script>
-<script src="{{ asset('backend/js/plugin/select2/select2.min.js') }}"></script>
+<link rel="stylesheet" type="text/css" href="{{{ asset('loopeer/quickcms/js/blueimp/css/jquery.fileupload-ui.css') }}}">
+<link rel="stylesheet" type="text/css" href="{{{ asset('loopeer/quickcms/js/blueimp/css/blueimp-gallery.min.css') }}}">
+<script src="{{ asset('loopeer/quickcms/js/blueimp/tmpl.min.js') }}"></script>
+<script src="{{ asset('loopeer/quickcms/js/blueimp/load-image.min.js') }}"></script>
+<script src="{{ asset('loopeer/quickcms/js/blueimp/canvas-to-blob.min.js') }}"></script>
+<script src="{{ asset('loopeer/quickcms/js/blueimp/bootstrap-image-gallery.min.js') }}"></script>
+<script src="{{ asset('loopeer/quickcms/js/blueimp/jquery.iframe-transport.js') }}"></script>
+<script src="{{ asset('loopeer/quickcms/js/blueimp/jquery.fileupload.js') }}"></script>
+<script src="{{ asset('loopeer/quickcms/js/blueimp/jquery.fileupload-process.js') }}"></script>
+<script src="{{ asset('loopeer/quickcms/js/blueimp/jquery.fileupload-image.js') }}"></script>
+<script src="{{ asset('loopeer/quickcms/js/blueimp/jquery.fileupload-validate.js') }}"></script>
+<script src="{{ asset('loopeer/quickcms/js/blueimp/jquery.fileupload-ui.js') }}"></script>
+<script src="{{ asset('loopeer/quickcms/js/plugin/select2/select2.min.js') }}"></script>
 <script>
     $(document).ready(function() {
         pageSetUp();
