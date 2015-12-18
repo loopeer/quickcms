c39293998ce8a72993dee806805a0778d6a38be2
dengyongbin <dengyongbin@loopeer.com>
2015-12-18 10:32:24 +0800

    update user

--- database/migrations/2015_09_11_033804_entrust_setup_tables.php
+++ database/migrations/2015_09_11_033804_entrust_setup_tables.php
@@ -35,10 +35,10 @@ class EntrustSetupTables extends Migration
             $table->integer('user_id')->unsigned();
             $table->integer('role_id')->unsigned();
 
-            $table->foreign('user_id')->references('id')->on('users')
-                ->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('role_id')->references('id')->on('roles')
-                ->onUpdate('cascade')->onDelete('cascade');
+//            $table->foreign('user_id')->references('id')->on('users')
+//                ->onUpdate('cascade')->onDelete('cascade');
+//            $table->foreign('role_id')->references('id')->on('roles')
+//                ->onUpdate('cascade')->onDelete('cascade');
 
             $table->primary(['user_id', 'role_id']);
         });
@@ -62,10 +62,10 @@ class EntrustSetupTables extends Migration
             $table->integer('permission_id')->unsigned();
             $table->integer('role_id')->unsigned();
 
-            $table->foreign('permission_id')->references('id')->on('permissions')
-                ->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('role_id')->references('id')->on('roles')
-                ->onUpdate('cascade')->onDelete('cascade');
+//            $table->foreign('permission_id')->references('id')->on('permissions')
+//                ->onUpdate('cascade')->onDelete('cascade');
+//            $table->foreign('role_id')->references('id')->on('roles')
+//                ->onUpdate('cascade')->onDelete('cascade');
 
             $table->primary(['permission_id', 'role_id']);
         });
