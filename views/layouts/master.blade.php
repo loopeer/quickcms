4da6bf1f1242aaf25a33fef7d4cdc96672f23a97
dengyongbin <dengyongbin@loopeer.com>
2016-04-01 12:34:59 +0800

    add layout style

--- views/layouts/master.blade.php
+++ views/layouts/master.blade.php
@@ -7,6 +7,7 @@
     <meta name="author" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     @include('backend::layouts.link')
+    @yield('style')
 </head>
 <body class="{{ config('quickcms.admin_body_layout') }}">
 @include('backend::layouts.header')
