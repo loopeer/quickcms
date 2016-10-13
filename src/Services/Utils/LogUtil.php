28505984858432df29fb6c0867f246926deadcd0
miracle <54miracle@gmail.com>
2016-10-13 23:13:12 +0800

    make the auto-create log file permission to 664

--- src/Services/Utils/LogUtil.php
+++ src/Services/Utils/LogUtil.php
@@ -31,7 +31,7 @@ class LogUtil {
             $file_name = $name.'.log';
         }
         $path = storage_path() . '/logs/' . ($dir ? ($dir . '/') : '') . $file_name;
-        $stream = new StreamHandler($path, Logger::INFO);
+        $stream = new StreamHandler($path, Logger::INFO, true, 0664);
         $fire_php = new FirePHPHandler();
         $logger->pushHandler($stream);
         $logger->pushHandler($fire_php);
