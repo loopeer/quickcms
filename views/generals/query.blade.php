54802101f96dc38a2f4711f30fcdf88e71eb4ac7
dengyongbin <dengyongbin@loopeer.com>
2017-01-22 15:43:40 +0800

    update query date input

--- views/generals/query.blade.php
+++ views/generals/query.blade.php
@@ -38,9 +38,9 @@
                                             <div class="input-group">
                                                 @if(isset($query_value['operator']) && $query_value['operator'] == 'between')
                                                     <input type="text" class="form-control date-format between" id="{{ $query_value['column'] . '_from' }}">
-                                                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                                    <span class="input-group-addon"></span>
                                                     <input type="text" class="form-control date-format between" id="{{ $query_value['column'] . '_to' }}">
-                                                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                                    <span class="input-group-addon"></span>
                                                 @else
                                                     <input type="text" class="form-control date-format single" id="{{ $query_value['column'] }}">
                                                     <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
