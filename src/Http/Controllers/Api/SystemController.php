996395a307a66d3cac44e46715515cdad90c4d64
lijiashuai <lijiashuai@loopeer.com>
2017-04-07 10:28:29 +0800

    change the  initialize api system_value to value

--- src/Http/Controllers/Api/SystemController.php
+++ src/Http/Controllers/Api/SystemController.php
@@ -44,8 +44,8 @@ class SystemController extends BaseController {
         $build_system = Cache::rememberForever('build_system', function () {
             return System::where('key', 'build')->first();
         });
-        if (count($review_system) > 0 && $review_system->system_value == 1) {
-            if (count($build_system) > 0 && $build_system->system_value == $version_code) {
+        if (count($review_system) > 0 && $review_system->value == 1) {
+            if (count($build_system) > 0 && $build_system->value == $version_code) {
                 $appstore_reviewing = true;
             }
         }
