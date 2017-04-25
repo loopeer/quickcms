42820bf21e2f7490b0cb079e3b3c1eaecafe915d
dengyongbin <dengyongbin@loopeer.com>
2017-04-25 15:44:37 +0800

    update captcha review bug

--- src/Http/Controllers/Api/AccountController.php
+++ src/Http/Controllers/Api/AccountController.php
@@ -394,12 +394,8 @@ class AccountController extends BaseController {
     private function isReviewing() {
         $version_code = \Request::header('build');
         $appstore_reviewing = false;
-        $review_system = Cache::rememberForever('review_system', function () {
-            return System::where('key', 'app_review')->first();
-        });
-        $build_system = Cache::rememberForever('build_system', function () {
-            return System::where('key', 'build')->first();
-        });
+        $review_system = System::where('key', 'app_review')->first();
+        $build_system = System::where('key', 'build')->first();
         if (count($review_system) > 0 && $review_system->value == 1) {
             if (count($build_system) > 0 && $build_system->value == $version_code) {
                 $appstore_reviewing = true;
